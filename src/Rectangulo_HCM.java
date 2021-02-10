54ca70b07bec7034a8b7190e2c9289d90d9fa9e5
Hector Cano <hectorcanomaestre@hotmail.com>
2021-02-10 12:19:49 +0100

    versión comentada

--- src/Rectangulo_HCM.java
+++ src/Rectangulo_HCM.java
@@ -1,19 +1,39 @@
 
+/**
+ * @author Héctor Cano Maestre
+ * @version 1.2
+ */
 public class Rectangulo_HCM extends FiguraGeometrica_HCM {
 	private double l1;
 	private double l2;
 	
+	
+	/**
+	 * Constructor de la clase. Asigana los valores al rectángulo.
+	 * Llama al constructor de FiguraGeometrica_HCM
+	 * @param tipoFigura String tipo figura.
+	 * @param lG lado grande.
+	 * @param lP lado pequeño.
+	 */
 	public Rectangulo_HCM(String tipoFigura, double lG, double lP) {
 		super(tipoFigura);
 		l1 = lG;
 		l2 = lP;
 	}
-
+	
+	/**
+	 * Método sobre-escrito que calcula el area del rectángulo.
+	 * @return Devuelve el area del rectángulo.
+	 */
 	@Override
 	public double area() {
 		return l1 * l2;
 	}
 	
+	/**
+	 * Método sobre-escrito que devuelve el perímetro del rectángulo.
+	 * @return Devuelve el perímetro del rectángulo.
+	 */
 	@Override
 	public double perimetro() {
 		return (2 * l1) + (2 * l2);
