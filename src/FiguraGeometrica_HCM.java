d60eac8d574269a59df3f50acdaccc1adb460e7d
Hector Cano <hectorcanomaestre@hotmail.com>
2021-02-10 12:07:00 +0100

    versión refacorizada

--- src/FiguraGeometrica_HCM.java
+++ src/FiguraGeometrica_HCM.java
@@ -1,9 +1,9 @@
 
-public abstract class FiguraGeometrica {
+public abstract class FiguraGeometrica_HCM {
 
 	protected String tipoFigura;
 
-	public FiguraGeometrica(String tipoFigura) {
+	public FiguraGeometrica_HCM(String tipoFigura) {
 		super();
 		this.tipoFigura = tipoFigura;
 	}
